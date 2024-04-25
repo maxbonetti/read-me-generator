a63ff33533f74728b67934668453812d7c1e92ba
Max Bonetti <maxwell.bonetti@gmail.com>
2024-04-24 19:37:22 -0500

    complete challenege and functional.

---
+++ Develop/README.md
@@ -0,0 +1,19 @@
+
+    # Test
+    ## Description
+    test
+    ## Installation Process
+    test
+    ## Usage
+    test
+    ## Contribution's
+    test
+    ## How to Test
+    test
+    ## License's 
+    MIT
+    ### GitHub
+    tst
+    ## Questions
+For any questions, please contact me at [test](mailto:test).
+    
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,97 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require(`inquirer`);
+const fs = require(`fs`);
+const colors = require(`colors`);
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = [
+    {
+        type:'input',
+        name:'title',
+        message:'What is the title of your project?'.bgBlack.white
+    },
+    {
+        type:'input',
+        name:'description',
+        message:'Provide a brief description of your project:'
+    },
+    {
+        type:'input',
+        name:'installation',
+        message:'How is your project installed?'
+    },
+    {
+        type:'input',
+        name:'usage',
+        message:'How is your project used?',
+    },
+    {
+        type:'input',
+        name:'contributing',
+        message:'How can others contribute to your project?',
+    },
+    {
+        type:'input',
+        name:'test',
+        message:'How is your project tested?'
+    },
+    {
+        type:'list',
+        name:'license',
+        message:'Choose a license for your project:',
+        choices:['MIT','GPL 3.0','Apache License 2.0','None']
+    },
+    {
+        type:'input',
+        name:'github',
+        message:'Enter your GitHub username:'
+    },
+    {
+        type:'input',
+        name:'email',
+        message:'Enter your email address:'
+    }
+];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data, err => {
+        if (err) {
+            console.error(`Failed to write file ${err}`.red);
+        } else {
+            console.log(`Successfully`.green `created README.md!`.white.bgBlack);
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init(questions) {
+    inquirer
+    .prompt(questions)
+    .then(answers => {
+        const readmeContent = generateReadmeContent(answers);
+        writeToFile(`README.md`,readmeContent);
+    });
+}
 // Function call to initialize app
-init();
+init(questions);
+
+function generateReadmeContent(answers) {
+    return `
+    # ${answers.title}
+    ## Description
+    ${answers.description}
+    ## Installation Process
+    ${answers.installation}
+    ## Usage
+    ${answers.usage}
+    ## Contribution's
+    ${answers.contributing}
+    ## How to Test
+    ${answers.test}
+    ## License's 
+    ${answers.license}
+    ### GitHub
+    ${answers.github}
+    ## Questions
+    If you have any questions in regards to ${answers.title}, please contact me at [${answers.email}](mailto:${answers.email}).
+    `;
+}
